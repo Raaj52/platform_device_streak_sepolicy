363c66a230d1bf68749ecb8a155498d5c72bc7cc
HSgod <hsgodkontakt@gmail.com>
2020-08-11 13:04:05 +0000

    sepolicy: Adaptation for Streak
    Signed-off-by: HSgod <hsgodkontakt@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -16,20 +16,20 @@ endif
 endif
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/public
+    device/streak/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/private
+    device/streak/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/system
+    device/streak/sepolicy/common/dynamic \
+    device/streak/sepolicy/common/system
 else
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/vendor
+    device/streak/sepolicy/common/dynamic \
+    device/streak/sepolicy/common/vendor
 endif
 
 # Selectively include legacy rules defined by the products
--include device/lineage/sepolicy/legacy-common/sepolicy.mk
+-include device/streak/sepolicy/legacy-common/sepolicy.mk
